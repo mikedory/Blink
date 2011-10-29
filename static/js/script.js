f64ce833c5c94ad19d819289ee9242dc462070a3
Mike Dory <mjd@doryexmachina.com>
2011-10-29 15:24:44 -0400

    resziey

--- static/js/script.js
+++ static/js/script.js
@@ -5,7 +5,7 @@
 google.load("jquery", "1.6.4");
 google.setOnLoadCallback(function() {
 
-	$('.blinksize').textfill({ maxFontPixels: 400 })
+	$('.blinksize').textfill({ maxFontPixels: 200 })
 
 
 	// set me some intervals
