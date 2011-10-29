8c0159ae8e0cfbd21a11d1a4cf7d6a35dda8c6e6
Mike Dory <mjd@doryexmachina.com>
2011-10-29 17:40:37 -0400

    inline documentation is our friend!

--- static/js/script.js
+++ static/js/script.js
@@ -1,20 +1,24 @@
-/* Author: Michael Dory
-
+/* 
+	Author: Michael Dory
 */
+
 // load jquery, then set the onload event
 google.load("jquery", "1.6.4");
 google.setOnLoadCallback(function() {
 
+	// Change this number to determine how big you want 
+	// the blinky text max font size to get
 	$('.blinksize').textfill({ maxFontPixels: 700 })
 
 
-	// set me some intervals
+	// Change this to set the text blink rate
 	setInterval(function() {
 
 		$('.blink').toggle();
 
 	}, 500);
 
+	// Change this to set the page background color change rate
 	setInterval(function() {
 
 		var randomnumber=Math.floor(Math.random()*999)
@@ -22,12 +26,11 @@ google.setOnLoadCallback(function() {
 
 	}, 200);
 
-	// change the color to something
+	// change the body's background color
 	function changeBodyColor(color) {
 
-    $("body").css({'background-color':color})
+		$("body").css({'background-color':color})
 		
 	}
-	
 
 });
